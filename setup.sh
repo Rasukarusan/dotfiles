270b3b02c780108b783e11028854ec0af4b31b76
Tanaka Naoto <gta_youtube@yahoo.co.jp>
2019-10-20 22:52:33 +0900

    setup.shにnvimを追加

--- setup.sh
+++ setup.sh
@@ -3,3 +3,5 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.xvimrc ~/.xvimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.init.vim ~/.config/nvim/init.vim
+ln -sf ~/dotfiles/.dein.toml ~/.config/nvim/dein.toml.vim
