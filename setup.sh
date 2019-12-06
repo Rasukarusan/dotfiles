ae381c33466ad310a036f32069235e1ac7bed29f
Tanaka Naoto <gta_youtube@yahoo.co.jp>
2019-12-06 10:39:11 +0900

    Revert "Delete .vimrc"
    This reverts commit 7246928b19110e01369140d8213c99aa3223abd9.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ln -sf ~/dotfiles/init.vim ~/.vimrc
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.xvimrc ~/.xvimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
