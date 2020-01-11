1b6805dc49ce6bc6e77a53a32d8a74014f6b5b57
Tanaka Naoto <gta_youtube@yahoo.co.jp>
2020-01-11 23:19:01 +0900

    add hyper.js simb-link

--- setup.sh
+++ setup.sh
@@ -7,3 +7,4 @@ ln -sf ~/dotfiles/init.vim ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/dein.toml ~/.config/nvim/dein.toml
 ln -sf ~/dotfiles/dein_tomls ~/.config/nvim/dein_tomls
 ln -sf ~/dotfiles/package.json ~/.config/coc/extensions/package.json
+ln -sf ~/dotfiles/.hyper.js ~/.hyper.js
