98e1ea9972bf5f1aa61cf5c0c0d22d6f04e382a7
Tanaka Naoto <gta_youtube@yahoo.co.jp>
2020-02-20 10:31:26 +0900

    remove defaultsCommands.sh and write to ansible
    refs #master

--- terminal/defaultsCommands.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#==================================================#
-# Xcode                                            #
-#==================================================#
-# Full screen mode enable
-defaults write com.apple.iphonesimulator AllowFullscreenMode -bool YES
-# Show tap place
-defaults write com.apple.iphonesimulator ShowSingleTouches -bool YES
