b0df8c495f4dbd289a9537ba5493d8b2c0821633
Tanaka Naoto <gta_youtube@yahoo.co.jp>
2020-02-29 00:23:31 +0900

    update initial.sh

--- initial.sh
+++ initial.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
+exists() {
+    type "$1" >/dev/null 2>&1
+}
+
 # Install Homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if exists breww ; then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
 # Install Ansible
-brew install ansible
-cd ansible && ansible-playbook -i inventory/localhost localhost.yml
+if exists ansible ; then
+    brew install ansible
+    cd ansible && ansible-playbook -i inventory/localhost localhost.yml
+fi
