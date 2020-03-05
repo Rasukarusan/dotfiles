8670cebe958ff0af699396ad60473250b87c1bc2
Tanaka Naoto <gta_youtube@yahoo.co.jp>
2020-03-05 15:54:15 +0900

    Add font Ricty-DiminishedDiscord

--- terminal/hyper.js
+++ terminal/hyper.js
@@ -12,7 +12,7 @@ module.exports = {
     fontSize: 12,
 
     // font family with optional fallbacks
-    fontFamily: 'Ricty, Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+    fontFamily: 'RictyDiminishedDiscord-Regular, Ricty, Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
     // default font weight: 'normal' or 'bold'
     fontWeight: 'normal',
